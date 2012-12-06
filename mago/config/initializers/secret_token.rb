af55e3a9afac070ee8255be0027f822d525c48d9
Vergon <vergonv@gmail.com>
2012-12-06 22:24:05 +0400

    Test owen

---
+++ mago/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mago::Application.config.secret_token = '2d0629d69dffa40df0d7da98364e209be2fad4479eec03059affcd851e60ad329dfc736a8c59670e24ee3604474323edf536ff39ba9bfb2b4ed42c84f2de01bb'
---
+++ mago/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mago::Application.config.session_store :cookie_store, key: '_mago_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mago::Application.config.session_store :active_record_store
