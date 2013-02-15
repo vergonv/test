e94477e6d0ea517b88a8bcc067cec30249ba47a3
Vergon <vergonv@gmail.com>
2013-02-15 23:17:42 +0400

    NA4alo

---
+++ tick/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tick::Application.config.secret_token = '43903681115ed0665a5419cb0e14169c30a2295a0a61fa5f7695015fe612031d49cd8e8d05ca730dd7f7ab0e29a6d9c87a0a31fa57a55a6900d071d92644a568'
