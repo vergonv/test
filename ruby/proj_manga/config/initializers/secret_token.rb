44575b9f26f8736e025542c6d01addb9b51b2893
Vergon <vergonv@gmail.com>
2013-10-11 11:41:48 +0400

    Start proj

---
+++ ruby/proj_manga/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjManga::Application.config.secret_key_base = 'befb3fa5ba1b9a6acda28ad1f248cfc787b4b60e9281fb374c42b37e4376be96a7ccd1f13de8edd7172c0bcac08585e4a80f23b444ea169f419eb23042add672'
---
+++ ruby/proj_manga/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjManga::Application.config.session_store :cookie_store, key: '_proj_manga_session'
